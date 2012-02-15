4715a204328e24daee4826e57ff4c49e3da8a4d5
Aljoscha Krettek <aljoscha.krettek@googlemail.com>
2012-02-15 23:41:35 +0100

    Reimplemented parser to use only streams and only read input once

--- parser.hpp
+++ parser.hpp
@@ -10,15 +10,15 @@
 #include <fstream>
 #include <string>
 #include <climits>
-#include <boost/xpressive/xpressive.hpp>
-#include <boost/lexical_cast.hpp>
-#include <boost/algorithm/string.hpp>
-
-
-static std::string NODE_PATTERN = "\\d+\\s*,?\\s*\\(\\s*\\-?\\s*\\d+\\s*,\\s*\\-?\\s*\\d+\\s*\\)(\\s*,?\\s*\\-?\\s*\\d+)?";
+#include <cctype>
 
 using namespace std;
-using namespace boost::xpressive;
+
+#define STATE_NODE 0
+#define STATE_NODE_P_OR_D 1
+#define STATE_NODE_P 2
+#define STATE_NODE_D_OR_END 3
+#define STATE_NODE_D 4
 
 // Reads a network instance from the given file and stores it in a new network
 // of type NetworkType. The network type must have a constructor that takes one
@@ -35,111 +35,88 @@ using namespace boost::xpressive;
 template <class NetworkType>
 NetworkType* parse_nwk(std::string filename) {
     NetworkType *network = NULL;
-    //sregex rex = sregex::compile(NODE_PATTERN.c_str());
-    string line;
     ifstream file(filename.c_str());
 
+    int num_arcs = 0;
     if (file.is_open()) {
-        getline(file, line);
-        int num_nodes = boost::lexical_cast<int>(line);
-        //std::cout << "Num nodes: " << num_nodes << std::endl;
-
-        // create network
+        int num_nodes;
+        file >> num_nodes;
+        //cout << "NUM_NODES: " << num_nodes << endl;
         network = new NetworkType(num_nodes);
 
-        int currline = 2;
+        // read away the newline
+        while (file.peek() != '\n') file.get();
+        file.get();
+
         while (file.good()) {
-            getline (file,line);
-            if (line.size() > 0) {
-                std::vector<std::string> line_splits;
-                boost::split(line_splits, line, boost::is_any_of(":"));
+            string line_string;
+            getline(file, line_string);
+            if (line_string.size() == 0) continue;
+            istringstream line(line_string);
 
-                smatch match;
+            // read the node number
+            int node_num;
+            int demand = 0;
+            line >> node_num;
 
-				// parse node data
-                std::string node_data = line_splits[0];
+            // read away whitespace
+            while (line.peek() == ' ' || line.peek() == '\t') line.get();
 
-                /*
-                if( !regex_search( node_data, match, rex ) )
-                {
-                    std::cout << "Illegal node description in line: "
-                        << currline << std::endl;
-                    return NULL;
-                }
-                */
-
-                std::vector<std::string> node_parts;
-                boost::split(node_parts, node_data, boost::is_any_of(","));
-
-                int node_num = -1;
-                int demand = 0;
-
-                std::vector<std::string> num_parts;
-                boost::split(num_parts, node_parts[0], boost::is_any_of(" "));
-
-                string num_string = num_parts[0];
-                node_num = boost::lexical_cast<int>(num_string);
-
-                if (node_parts.size() > 2) {
-                    string demand_string = node_parts[2];
-                    boost::algorithm::trim(demand_string);
-                    demand = boost::lexical_cast<int>(demand_string);
-                }
-                
-                //std::cout << "Node: " << node_num << " " << demand << std::endl;
-                network->add_node(node_num, demand);
-
-
-				// parse incident arcs
-				if (line_splits.size() > 1) {
-					string arcs_data = line_splits[1];
-
-                    std::vector<std::string> arcs;
-                    boost::split(arcs, arcs_data, boost::is_any_of(";"));
-
-					for (unsigned int i = 0; i < arcs.size(); ++i) {
-                        boost::algorithm::trim(arcs[i]);
-
-                        std::vector<std::string> arc_data;
-                        boost::split(arc_data, arcs[i], boost::is_any_of(","));
-
-                        if (arc_data.size() == 0) {
-                            continue;
-                        }
-
-						if (arc_data.size() < 2) {
-                            //std::cout << "Illegal arc desc: " << arcs[i] << std::endl;
-                            continue;
-                        }
-
-                        string target_string = arc_data[0];
-                        boost::algorithm::trim(target_string);
-                        int target_node = boost::lexical_cast<int>(target_string);
-
-                        if (target_node >= num_nodes) {
-                            std::cout << "Illegal target node: " << target_node << std::endl;
-                        }
-
-                        boost::algorithm::trim(arc_data[1]);
-                        long cost = boost::lexical_cast<int>(arc_data[1]);
-
-                        long capacity = LONG_MAX;
-
-                        if (arc_data.size() > 2) {
-                            boost::algorithm::trim(arc_data[2]);
-                            capacity = boost::lexical_cast<int>(arc_data[2]);
-                        }
-
-                        // add new arc
-                        //std::cout << "Arc: " << target_node << " " << cost <<
-                        //    " " << capacity << std::endl;
-                        network->add_arc(node_num, target_node, cost, capacity);
-					}
-				}
+            if (line.peek() == '(') {
+                // skip to the demand
+                while (line.peek() != ')') line.get();
+                line.get();
             }
-            ++currline;
+
+            if (line.peek() == ',') {
+                line.get();
+                // read away whitespace
+                while (line.peek() == ' ' || line.peek() == '\t') line.get();
+                line >> demand;
+            }
+
+            //cout << "node: " << node_num << " " << demand << endl;
+            network->add_node(node_num, demand);
+            
+
+            while (line.peek() != ':') line.get();
+            line.get();
+
+            //cout << "READING ARCS\n";
+
+            // now the arcs ...
+            
+            while (line.good()) {
+                // read away whitespace
+                while (line.peek() == ' ' || line.peek() == '\t') line.get();
+                if (!line.good()) break;
+
+                int target;
+                line >> target;
+
+                // read ", "
+                line.get();
+                line.get();
+
+                int cost;
+                line >> cost;
+
+                int capacity = LONG_MAX;
+                if (line.peek() != ';') {
+                    // read ", "
+                    line.get();
+                    line.get();
+                    line >> capacity;
+                }
+                line.get();
+                
+                //cout << "ARC: " << target << " " << cost << " " << capacity << endl; 
+                network->add_arc(node_num, target, cost, capacity);
+                ++num_arcs;
+            }
+
+
         }
-        file.close();
     } else {
         cout << "Unable to open file " << filename;
     }
